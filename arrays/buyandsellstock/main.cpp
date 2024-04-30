2bdffc3e0f39cad013796dbb84d0e3ff61ad8d39
Takaya <119724608+santavalleytea@users.noreply.github.com>
2024-04-30 23:20:33 +1000

    Create main.cpp

---
+++ arrays/buyandsellstock/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < size(prices); i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+
+        return profit;
+    }
+};
