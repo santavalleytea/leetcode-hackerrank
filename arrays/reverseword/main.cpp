ffbdb134792764afdf457dce9a3b99d0ac44bccd
Takaya <119724608+santavalleytea@users.noreply.github.com>
2024-05-01 15:12:27 +1000

    Create main.cpp

---
+++ arrays/reverseword/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for (int i = 0; i < word.length(); ++i) {
+            if (word[i] == ch) {
+                reverse(word.begin(), word.begin() + i + 1);
+                break;
+            }
+        }
+
+        return word;
+    }
+};
