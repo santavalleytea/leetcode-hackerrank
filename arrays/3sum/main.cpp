5939c29580ac047ab4a05b29aacfcc4652968573
Takaya <119724608+santavalleytea@users.noreply.github.com>
2024-06-21 15:33:21 +1000

    Create main.cpp

---
+++ arrays/3sum/main.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    // Skip duplicates for pointers
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        ++left;
+                    }
+
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        --right;
+                    }
+
+                    ++left;
+                    --right;
+                } else if (sum < 0) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+        }
+
+        return result;
+
+    }
+};
