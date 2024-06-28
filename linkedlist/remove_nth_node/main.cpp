cb5b8ba2341d7bd613c59c47c5654ef881de2280
Takaya <119724608+santavalleytea@users.noreply.github.com>
2024-06-28 16:42:32 +1000

    Create main.cpp

---
+++ linkedlist/remove_nth_node/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode temp(-1);
+        temp.next = head;
+        ListNode* first = &temp;
+        ListNode* second = &temp;
+
+        // Ensure gap between first and second pointer is n nodes
+        for (int i = 0; i <= n; ++i) {
+            first = first->next;
+        }
+
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+
+        ListNode* toDelete = second->next;
+        second->next = second->next->next;
+
+        return temp.next;
+    }
+};
